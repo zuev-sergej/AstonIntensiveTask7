ca478efda1f0085374cba97151ca41d2c4e06d7d
Zuev_Sergey <zuevkajo@gmail.com>
2026-09-17 19:38:45 +0300

    Initial commit

---
+++ src/main/java/ru/aston/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.aston.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
